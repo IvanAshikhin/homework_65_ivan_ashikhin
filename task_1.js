1e5ac3cb99e74b7fff386a359f9f600e885fb5a0
Ivan <ashikhin1998@gmail.com>
2023-03-30 21:37:26 +0600

    Первое задание

---
+++ task_1.js
@@ -0,0 +1,8 @@
+let task = ['молоко', 'пиво', 'пиво', 'молоко', 'молоко'];
+for (let i = 0; i < task.length; i++) {
+  if (task[i] === 'пиво') {
+    console.log('хорошо');
+  } else {
+    console.log('плохо');
+  }
+}
